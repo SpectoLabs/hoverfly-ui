2d620a2d7d8682c7693367e5d9794ab4bd399e1a
Tommy Situ <tommy.situ@specto.io>
2017-05-25 11:10:13 +0100

    Fix component tests

--- src/app/topnavbar/topnavbar.module.ts
+++ src/app/topnavbar/topnavbar.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TopnavbarComponent } from './topnavbar.component';
 import { RouterModule } from '@angular/router';
+import { HoverflyService } from "../shared/services/hoverfly.service";
+import { HttpModule } from "@angular/http";
+import { AuthService } from "../shared/services/auth.service";
 
 @NgModule({
   declarations: [ TopnavbarComponent ],
-  imports: [ BrowserModule, RouterModule ],
+  imports: [ BrowserModule, RouterModule, HttpModule],
   exports: [ TopnavbarComponent ],
+  providers: [
+    HoverflyService,
+    AuthService
+  ]
 })
 
 export class TopnavbarModule {
